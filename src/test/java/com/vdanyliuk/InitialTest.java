c3874d9dcf4cd8b7c00a79bce1d5b71c1094db64
DrBAXA <DrBAXA@hotmail.com>
2016-04-23 22:16:44 +0300

    maven updated

--- src/test/java/com/vdanyliuk/InitialTest.java
+++ src/test/java/com/vdanyliuk/InitialTest.java
@@ -7,8 +7,13 @@ import org.junit.Test;
 public class InitialTest {
 
     @Test
-    public void add() throws Exception {
+    public void testAdd() throws Exception {
         TestCase.assertEquals(4, Initial.add(2, 2));
+    }
+
+    @Test
+    public void testAddOverload() throws Exception {
+        TestCase.assertEquals(-1, Initial.add(Integer.MAX_VALUE, 1));
     }
 
 }
