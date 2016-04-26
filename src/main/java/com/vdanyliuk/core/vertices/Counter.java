4a54de65d7a6dec3d9a21f74ad8f289064bbe03d
vdanyliuk <vdanyliuk@softjourn.com>
2016-04-26 12:40:09 +0300

    IsComputed method added
    added default method to Vertex interface that return TRUE value by default.
     In Counter class this method overridden to return false( such as counter get real electricity amount)

--- src/main/java/com/vdanyliuk/core/vertices/Counter.java
+++ src/main/java/com/vdanyliuk/core/vertices/Counter.java
@@ -19,6 +19,11 @@ public class Counter implements Vertex {
     private boolean reverse;
 
     @Override
+    public boolean isComputed() {
+        return false;
+    }
+
+    @Override
     public long getIncoming() {
         return reverse ? Aplus : Aminus;
     }
--- src/main/java/com/vdanyliuk/core/vertices/Vertex.java
+++ src/main/java/com/vdanyliuk/core/vertices/Vertex.java
@@ -6,13 +6,31 @@ package com.vdanyliuk.core.vertices;
 public interface Vertex {
 
     /**
+     * Define if incoming and outgoing data is computed
+     * or gotten from real counter device
+     * <p>
+     * Should return false if data is gotten form some storage
+     * and true if is computed based on other network elements
+     * <p>
+     * In child classes that get data from real device
+     * should be overridden to return false
+     *
+     * @return true by default.
+     */
+    default boolean isComputed() {
+        return true;
+    }
+
+    /**
      * Represent incoming amount of energy that was transferred through this vertex
+     *
      * @return incoming amount of energy in kW
      */
     long getIncoming();
 
     /**
      * Represent outgoing amount of energy that was transferred through this vertex
+     *
      * @return outgoing amount of energy in kW
      */
     long getOutgoing();
